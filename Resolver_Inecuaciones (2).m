4eb82f49fcd0403612621b11be6501750c298936
Orlando Urbano Trejo <91504341+OrlandoUrbanoTrejov@users.noreply.github.com>
2021-10-07 21:55:32 -0500

    Edite Código
    Colabore en el script haciendo cambios en el código, de el tema de inecuaciones.

--- Resolver_Inecuaciones (2).m
+++ Resolver_Inecuaciones (2).m
@@ -1,11 +1,11 @@
 % Octave Script
 % Title                      :Resolver_inecuaciones
 % Description                :Script para resolver inecuaciones de primer y segundo grado con una incognita
-% Author                     :Gustavo García Barrera(NONE_NAME)
+% Author                     :Gustavo GarcÃ­a Barrera(NONE_NAME)
 % Date                       :20211007
 % Version                    :1     
 % Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
-%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :Requiere aplicaciÃ³n octave para usar su lÃ­nea de comandos
 %                            :https://octaveintro.readthedocs.io/en/latest/index.html
 
 %inecuacion de primer grado 
@@ -36,8 +36,8 @@ ezplot (f)
 
 syms x
 disp '------inecuacion con valor absoluto----'
-disp 'abs (x+8)<7'
-solve (abs (x+8)<7)
+disp''abs (x+8)<7 
+solve (abs (x+8)<7)  
 
 f=x+1
 subplot (3,3,3)
