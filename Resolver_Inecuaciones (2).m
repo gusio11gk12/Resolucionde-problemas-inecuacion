98eaadc48447e29761c1379fb9883cbbf9d03b3f
aviescobar <91568563+aviescobar@users.noreply.github.com>
2021-10-11 18:23:53 -0500

    Propuesta de cambio para Scrip
    Propuesta de comentario para mejor del Scrip de nuestro grupo

--- Resolver_Inecuaciones (2).m
+++ Resolver_Inecuaciones (2).m
@@ -43,5 +43,45 @@ f=x+1
 subplot (3,3,3)
 ezplot (f)
 
+% Octave Script
+% Title                      :Resolver_inecuaciones
+% Description                :Script para resolver inecuaciones de primer y segundo grado con una incognita
+% Author                     :Victoria Avigail Escobar Noguez (Avi Escobar)
+% Date                       :20211110
+% Version                    :1     
+% Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
+%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :https://octaveintro.readthedocs.io/en/latest/index.html
+
+%inecuacion de primer grado 
+pkg load symbolic
+syms x
+disp '------inecuacion de primer grado----'
+disp 'x-7>4';
+solve (x-7 > 4)
+disp 'su intervalo es:(11,oo)'
+syms x
+f=x-11
+subplot(3,3,1);
+ezplot (f)
+syms x
+disp '------inecuacion de segundo grado----'
+disp 'x^2+15<10'
+solve (x^2+14<25)
+disp 'su intervalo es:(-?11,?11)'
+syms x
+f=x^2+14-25
+subplot (3,3,2);
+ezplot (f)
+
+
+syms x
+disp '------inecuacion con valor absoluto----'
+disp''abs (x+8)<7 
+solve (abs (x+8)<7)  
+
+f=x+1
+subplot (3,3,3)
+ezplot (f)
 
 
